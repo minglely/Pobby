6d12eb3f0a8d5d899ff7d1d550d0a0e68e9b8997
mint553 <64180706+mint553@users.noreply.github.com>
2020-10-16 19:51:22 +0900

    Initial Commit

---
+++ Pobby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pobby
+//
+//  Created by 김민주 on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
